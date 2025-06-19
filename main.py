3315abbeec7d2e5af233dee677e582e76a9bcff7
dimoria <di.dubenok05@gmail.com>
2025-06-19 14:41:01 +0300

    developed main.py added interaction mode for more comfortable usage

--- main.py
+++ main.py
@@ -1,48 +1,80 @@
 from circular_linked_list import CircularLinkedList
 from array_list import ArrayList
 
-def demo_list_operations(lst):
-    print("Append 'a', 'b', 'c'")
-    lst.append('a')
-    lst.append('b')
-    lst.append('c')
-    print("Length:", lst.length())
+def print_menu():
+    print("\nМеню:")
+    print("1. Додати елемент")
+    print("2. Вставити елемент за індексом")
+    print("3. Отримати елемент за індексом")
+    print("4. Видалити елемент за індексом")
+    print("5. Видалити всі елементи зі значенням")
+    print("6. Знайти перший індекс значення")
+    print("7. Знайти останній індекс значення")
+    print("8. Перевернути список")
+    print("9. Клонувати список")
+    print("10. Розширити іншим списком")
+    print("11. Очистити список")
+    print("12. Вивести довжину")
+    print("13. Вийти")
 
-    print("Insert 'x' at index 1")
-    lst.insert('x', 1)
+def interactive_mode(lst):
+    while True:
+        print_menu()
+        choice = input("Оберіть опцію: ")
 
-    print("Get index 1:", lst.get(1))
-
-    print("Delete index 2:", lst.delete(2))
-    print("Find first 'a':", lst.findFirst('a'))
-    print("Find last 'a':", lst.findLast('a'))
-
-    print("Clone and reverse the list")
-    cloned = lst.clone()
-    cloned.reverse()
-    print("Cloned first element after reverse:", cloned.get(0))
-
-    print("Extend with ['y', 'z']")
-    if isinstance(lst, CircularLinkedList):
-        extension = CircularLinkedList()
-    else:
-        extension = ArrayList()
-    extension.append('y')
-    extension.append('z')
-    lst.extend(extension)
-
-    print("Delete all 'a'")
-    lst.deleteAll('a')
-
-    print("Clear list")
-    lst.clear()
-    print("Length after clear:", lst.length())
+        try:
+            if choice == '1':
+                val = input("Введіть символ: ")
+                lst.append(val)
+            elif choice == '2':
+                val = input("Символ: ")
+                idx = int(input("Індекс: "))
+                lst.insert(val, idx)
+            elif choice == '3':
+                idx = int(input("Індекс: "))
+                print("Елемент:", lst.get(idx))
+            elif choice == '4':
+                idx = int(input("Індекс: "))
+                print("Видалено:", lst.delete(idx))
+            elif choice == '5':
+                val = input("Значення: ")
+                lst.deleteAll(val)
+            elif choice == '6':
+                val = input("Значення: ")
+                print("Перший індекс:", lst.findFirst(val))
+            elif choice == '7':
+                val = input("Значення: ")
+                print("Останній індекс:", lst.findLast(val))
+            elif choice == '8':
+                lst.reverse()
+            elif choice == '9':
+                clone = lst.clone()
+                print("Клоновано. Довжина клону:", clone.length())
+            elif choice == '10':
+                print("Додавання ['x','y','z']")
+                ext = CircularLinkedList() if isinstance(lst, CircularLinkedList) else ArrayList()
+                for ch in ['x', 'y', 'z']:
+                    ext.append(ch)
+                lst.extend(ext)
+            elif choice == '11':
+                lst.clear()
+            elif choice == '12':
+                print("Довжина:", lst.length())
+            elif choice == '13':
+                break
+            else:
+                print("Невідома команда")
+        except Exception as e:
+            print("Помилка:", e)
 
 if __name__ == "__main__":
-    print("== CircularLinkedList Demo ==")
-    clist = CircularLinkedList()
-    demo_list_operations(clist)
+    print("Оберіть тип списку:")
+    print("1. Однозв'язний кільцевий список")
+    print("2. Список на масиві")
+    t = input("Ваш вибір: ")
+    if t == '1':
+        structure = CircularLinkedList()
+    else:
+        structure = ArrayList()
 
-    print("\n== ArrayList Demo ==")
-    alist = ArrayList()
-    demo_list_operations(alist)
+    interactive_mode(structure)
